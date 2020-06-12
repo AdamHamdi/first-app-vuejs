83f71f4ba591212e182831f06067ad3671be7339
Adam Hamdi <prodev.webdesign@gmail.com>
2020-06-12 19:24:44 +0100

    welcome message .vue

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="welcome-message">
+        <h1>Welcome to vue js course</h1>
+        <p>We are in the first lesson</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "WelcomeMessage"
+};
+</script>
+<style scoped>
+h1 {
+    font-size: 50px;
+}
+</style>
