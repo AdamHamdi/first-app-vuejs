7a9c916abf8a14607ecb4aa48eb436b68c7308f2
Adam Hamdi <prodev.webdesign@gmail.com>
2020-06-12 21:40:44 +0100

    AppHeader .vue

---
+++ src/components/global/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="header">
+        <div class="logo">
+            Adam Vue
+        </div>
+        <ul id="nav">
+            <router-link to="/" tag="li"><a>Home</a></router-link> |
+            <router-link to="/about" tag="li"><a>About</a></router-link> |
+            <router-link to="/help" tag="li"><a>Help</a></router-link>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+    name: "AppHeader"
+};
+</script>
---
+++ src/views/Help.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="our-help">
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quae
+            unde, sapiente ad, deleniti dolorum quod commodi nostrum ullam
+            necessitatibus veniam libero officia recusandae adipisci minus
+            excepturi. Eveniet, commodi aut!
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: "help"
+};
+</script>
